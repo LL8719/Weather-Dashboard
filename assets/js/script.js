0268f137d06f436a85058ff6b134559015c24416
LL8719 <arsgatslr_87@yahoo.com>
2023-02-04 21:01:34 -0700

    clened up code separating functions intead of having everythin in an object, Still needs work as search results are creating duplicates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,101 +1,112 @@
-var foreCast = {
-	apiKey: 'bb54b8b606113e3da27c5bafd590d42f',
-	todaysWeather: function (city) {
-		fetch(
-			'https://api.openweathermap.org/data/2.5/weather?q=' +
-				city +
-				'&units=imperial&appid=' +
-				this.apiKey
-		)
-			// .then((response) => response.json())
-			// 	.then((data) => console.log(data))
-
-			.then((response) => {
-				if (!response.ok) {
-					alert('No weather found.');
-					throw new Error('No weather found.');
-				}
-				return response.json();
-			})
-			.then((data) => {
-				this.makeButton(city);
-				this.forecastWeather(city);
-				this.displayWeather(data);
-			});
-	},
-	displayWeather: function (data) {
-		var name = data.name;
-		console.log(data);
-		var icon = data.weather[0].icon;
-		console.log(icon);
-		var temp = data.main.temp;
-		console.log(temp);
-		var humidity = data.main.humidity;
-		console.log(humidity);
-		var { speed } = data.wind;
-		console.log(speed);
-		$('.card-city').text('Weather in ' + name);
-		$('.card-icon').attr(
-			'src',
-			'https://openweathermap.org/img/wn/' + icon + '.png'
-		);
-		$('.card-temp').text(temp + ' °F');
-		$('.card-wind').text('Wind speed: ' + speed + ' MPH');
-		$('.card-humid').text('Humidity: ' + humidity + '%');
-	},
-	forecastWeather: function (city) {
-		fetch(
-			'https://api.openweathermap.org/data/2.5/forecast?q=' +
-				city +
-				'&units=imperial&appid=' +
-				this.apiKey
-		)
-			.then((res) => res.json())
-			.then((data) => {
-				const organizedArr = [];
-				for (i = 0; i < data.list.length; i++) {
-					if (data.list[i].dt_txt.includes('12:00:00')) {
-						organizedArr.push(data.list[i]);
-					}
-				}
-				this.displayForecast(organizedArr);
-			});
-	},
-	displayForecast: function (arr) {
-		console.log(arr);
-		for (i = 0; i < arr.length; i++) {
-			var newPDate = $('<h4>').text(arr[i].dt_txt.split(' ')[0]);
-			var newPTemp = $('<p>').text('Temperature: ' + arr[i].main.temp + ' °F');
-			var newPWind = $('<p>').text('Wind Speed: ' + arr[i].wind.speed + ' MPH');
-			var newPHumid = $('<p>').text('Humidity: ' + arr[i].main.humidity + '%');
-			var newIcon = $('<img>').attr(
-				'src',
-				'https://openweathermap.org/img/wn/' +
-					arr[i].weather[0].icon +
-					'@2x.png'
-			);
-			$('#box' + (i + 1)).append(newPDate);
-			$('#box' + (i + 1)).append(newPTemp);
-			$('#box' + (i + 1)).append(newPHumid);
-			$('#box' + (i + 1)).append(newPWind);
-			$('#box' + (i + 1)).append(newIcon);
-		}
-	},
-	makeButton: function (string) {
-		var newBtn = $('<button>').text(string);
-		newBtn.on('click', () => {
-			this.todaysWeather(newBtn.text());
+var apiKey = 'bb54b8b606113e3da27c5bafd590d42f ';
+//Todays weather from API
+function todaysWeather(city) {
+	var weatherUrl =
+		'https://api.openweathermap.org/data/2.5/weather?q=' +
+		city +
+		'&units=imperial&appid=' +
+		apiKey;
+	fetch(weatherUrl)
+		.then((response) => {
+			if (!response.ok) {
+				alert('No Weather Found!');
+			}
+			return response.json();
+		})
+		.then((data) => {
+			displayWeather(data);
+			forecastWeather(city);
 		});
+}
+//Display of weather on the main card for today
+function displayWeather(data) {
+	var name = data.name;
+	var icon = data.weather[0].icon;
+	var temp = data.main.temp;
+	var humidity = data.main.humidity;
 
-		$('#listOfCities').append(newBtn);
-	},
-	search: function () {
-		this.todaysWeather($('.search-input').val().trim());
-	},
-};
+	var wind = data.wind.speed;
 
-$('.search-button').on('click', function () {
-	foreCast.search();
+	$('.card-city').text('Weather in ' + name);
+	$('.card-icon').attr(
+		'src',
+		'https://openweathermap.org/img/wn/' + icon + '.png'
+	);
+	$('.card-temp').text(temp + ' °F');
+	$('.card-wind').text('Wind speed: ' + wind + ' MPH');
+	$('.card-humid').text('Humidity: ' + humidity + '%');
+}
+
+function forecastWeather(city) {
+	var forecastUrl =
+		'https://api.openweathermap.org/data/2.5/forecast?q=' +
+		city +
+		'&units=imperial&appid=' +
+		apiKey;
+	fetch(forecastUrl)
+		.then((response) => response.json())
+		.then((data) => {
+			var daysArr = [];
+			for (var i = 0; i < data.list.length; i++) {
+				if (data.list[i].dt_txt.includes('12:00:00')) {
+					daysArr.push(data.list[i]);
+				}
+			}
+			//loop over Data to append to 5 day weather cards
+			for (i = 0; i < daysArr.length; i++) {
+				var newPDate = $('<h4>').text(daysArr[i].dt_txt.split(' ')[0]);
+				var newPTemp = $('<p>').text(
+					'Temperature: ' + daysArr[i].main.temp + ' °F'
+				);
+				var newPWind = $('<p>').text(
+					'Wind Speed: ' + daysArr[i].wind.speed + ' MPH'
+				);
+				var newPHumid = $('<p>').text(
+					'Humidity: ' + daysArr[i].main.humidity + '%'
+				);
+				var newIcon = $('<img>').attr(
+					'src',
+					'https://openweathermap.org/img/wn/' +
+						daysArr[i].weather[0].icon +
+						'@2x.png'
+				);
+				$('#box' + (i + 1)).append(newPDate);
+				$('#box' + (i + 1)).append(newPTemp);
+				$('#box' + (i + 1)).append(newPHumid);
+				$('#box' + (i + 1)).append(newPWind);
+				$('#box' + (i + 1)).append(newIcon);
+			}
+		});
+}
+
+//local storage
+var searchHist = JSON.parse(localStorage.getItem('searchHistory')) || [];
+function saveCities() {
+	searchHist.push(city);
+	localStorage.setItem('searchHist', JSON.stringify(searchHist));
+}
+// Makes new button from user search
+function makeButton() {
+	//grabs the user input
+	city = $('#search-input').val().trim();
+	//creates button with user input
+	var newBtn = $('<button>').text(city);
+	//adds class for style
+	newBtn.addClass('btn  btn-primary');
+
+	newBtn.on('click', () => {
+		// todaysWeather(newBtn.text());
+	});
+	//displays button from search on the screen
+	$('#listOfCities').append(newBtn);
+}
+//Search button event allows for weather to change on main card
+$('.search-button').on('click', function (event) {
+	event.preventDefault();
+	city = $('#search-input').val().trim();
+	makeButton();
+	saveCities();
+	todaysWeather(city);
 });
 
-foreCast.todaysWeather('Phoenix');
+todaysWeather('Phoenix');
